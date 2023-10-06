b54bdaca0f992e11e35a4b33906edfc264b27b0c
Szymon Zymelka <szymelka@driv.com>
2023-10-06 14:00:58 +0200

    Test updated

--- test.py
+++ test.py
@@ -4,8 +4,7 @@ files = ['SignalExample.rsp']
 
 sig = RPC3(files[0], debug=False)
 sig.Channels[-1].plot()
-sig.Channels[-1].rainflow_counting()
-sig.Channels[-1].calculate_damage(5, gate=5, plot_graphs=True)
+
 
 # Save rsp file
 sig.save('OutputSignal.rsp', sig.dt, sig.Channels)
